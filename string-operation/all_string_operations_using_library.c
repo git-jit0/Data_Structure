11afee857eb08a88598ff01d7f280768ffa1e069
Jit Debnath <jitdebnathinkolkata@gmail.com>
2024-01-29 12:40:08 +0530

    changed some code here

--- string-operation/all_string_operations_using_library.c
+++ string-operation/all_string_operations_using_library.c
@@ -1,24 +1,12 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-      char name[200];
-      printf("Enter something: ");
-      fgets(name,sizeof(name),stdin);
-      printf("%s",name);
-
-      int length = strlen(name); // to measure the length of string
-      printf("\nLength of string %d",length);
-      int cmp = strcmp(name,name); // compareing the same string , will return 1 
-      printf("\ncompared string %d",cmp);
-      char new_name[200]; strcpy(new_name,name); // destination, source // to copy string
-      puts("\ncopied string");
-      puts(new_name);
-      strcat(name,new_name); // addes new_name after name
-      puts("\nconcaatinated string ");
-      puts(name);
-      strrev(new_name); //reverse the string
-      puts("\nreversed string ");
-      puts(new_name);
-      return 0;
-}
+// #include<string.h> // including this is necessary 
+// char name[200] , new_name[200] ; 
+// int length , compare;
+// printf("Enter something: ");
+// fgets(name,sizeof(name),stdin);
+// printf("%s",name);
+// length = strlen(name); // to get the length of the string
+// compare = strcmp(name,name); // to compare 2 strings
+// new_name[200]; strcpy(new_name,name); // to copy a string into another array
+//  strcat(name,new_name); // to add or merge two different string
+// strrev(new_name); // to reverse a string 
+      
