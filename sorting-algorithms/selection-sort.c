40599d426a165530cb37768b96d82579ae22205a
Jit Debnath <jitdebnathinkolkata@gmail.com>
2024-03-17 10:16:56 +0530

    added heapsort mergesort quicksort radixsort

--- sorting-algorithms/selection-sort.c
+++ sorting-algorithms/selection-sort.c
@@ -1,10 +1,3 @@
-/* Selection sort algorithm (for ascending order) 
-1. Find the minimum element in the array and swap it with the element in the 1st position. 
-2. Find the minimum element again in the remaining array[2, n] and swap it with the element 
-      at 2nd position, now we have two elements at their correct positions. 
-3. We must do this n-1 times to sort the array.
-4. time complexity is O(n^2) */
-
 #include <stdio.h>
 int main()
 {
