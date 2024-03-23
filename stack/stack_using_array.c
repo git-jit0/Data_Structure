bb0c74735cecc71ecaef7730d88e2616a1c56ec5
Jit Debnath <jitdebnathinkolkata@gmail.com>
2024-03-23 09:10:22 +0530

    added peek function

--- stack/stack_using_array.c
+++ stack/stack_using_array.c
@@ -6,18 +6,21 @@ struct stack {
     int top;
     int *arr;
 };
+
 int isempty(struct stack *ptr){
     if(ptr->top==-1)
         return 1;
     else 
         return 0;
 }
+
 int isfull(struct stack *ptr){
     if(ptr->top == ptr->size-1)
         return 1;
     else 
         return 0;
 }
+
 void push (int value , struct stack *ptr){
     if(isfull(ptr)){
         printf("\nStack Overflow!");
@@ -27,6 +30,7 @@ void push (int value , struct stack *ptr){
         ptr->top++;
     }
 }
+
 int pop (struct stack *ptr){
     if(isempty(ptr)){
         return -1; // considering the -1 value is not in stack 
@@ -35,6 +39,14 @@ int pop (struct stack *ptr){
         ptr->top--;
         return ptr->arr[ptr->top+1];
     }
+}
+
+int peek(struct stack *ptr , int i){
+    if(ptr->top-i+1<0)
+        return -1;
+
+    else 
+        return ptr->top-i+1;
 }
 int main(){
     struct stack *val = (struct stack *) malloc(sizeof(struct stack));
@@ -46,7 +58,12 @@ int main(){
     printf("\nstack is empty = %d",isempty(val));
 
     push(1,val);
+    push(2,val);
+    push(3,val);
+    push(4,val);
+    push(5,val);
     pop(val);
+    printf("\nPeek position 0 from top = %d",peek(val , 0));
 
     printf("\nstack is full = %d",isfull(val));
     printf("\nstack is empty = %d",isempty(val));
